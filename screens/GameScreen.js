8657547913a194007641485936b3bfbeba4b68e1
Erick Santos <erick.santos@direcional.com.br>
2024-05-09 18:46:01 -0300

    theme colors, gameScreen started, random number

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,49 @@
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Title from '../components/ui/Title';
+import Colors from '../constants/colors';
+import NumberContainer from '../components/game/NumberContainer';
+import { useState } from 'react';
 
-function GameScreen() {
-  return <Text>Game Screen!</Text>;
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  }
+  return rndNum;
+}
+
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* + - */}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: Colors.accent500,
+    textAlign: 'center',
+    borderWidth: 2,
+    borderColor: Colors.accent500,
+    padding: 12,
+  },
+});
